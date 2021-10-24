4ec300b97148d70c05aec6102d5323331ddac5a9
Leo van Haaren <leovhaaren@gmail.com>
2021-10-24 20:50:49 +0200

    fix: homepage button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Footer from '../components/footer';
+import Link from '../components/link';
 import Meta from '../components/meta';
 import Navigation from '../components/navigation';
 
@@ -19,7 +20,8 @@ export default function Home() {
               <div className="w-full lg:w-2/3 px-4 mb-12 lg:mb-0">
                 <div className="max-w-lg"><span className="text-xs text-tetriary-light font-semibold">Wherever your mind goes</span>
                   <h2 className="mt-8 mb-6 lg:mb-10 lg:pr-2 text-4xl font-bold font-heading">Remember everything you want, by the means you prefer, in the frequency you like</h2>
-                  <p className="mb-6 lg:mb-12 text-xl text-gray-500">Remindle is a platform which helps you to remember all important events. You can set the means you’d like to receive the notifications and adjust the frequency according to importance levels.</p><a className="inline-block py-4 px-8 text-sm text-white font-medium leading-normal bg-primary hover:bg-secondary transition duration-200 rounded transition duration-200" href="https://remindle.io/register">Join our community</a>
+                  <p className="mb-6 lg:mb-12 text-xl text-gray-500">Remindle is a platform which helps you to remember all important events. You can set the means you’d like to receive the notifications and adjust the frequency according to importance levels.</p>
+                  <Link className="inline-block py-4 px-8 text-sm text-white font-medium leading-normal bg-primary hover:bg-secondary transition duration-200 rounded" href="/register">Join our community</Link>
                 </div>
               </div>
               {/* <div className="w-full lg:w-1/2 px-4"><img className="rounded-xl lg:max-w-2xl" src="https://fakeimg.pl/675x500/ff0000,10" alt=""/></div> */}
